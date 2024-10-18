3b17d09d5a425f8749cff70e519742e94e57b6ad
Hugo Rojas Z <hugo.rojas1@mail.udp.cl>
2024-10-18 12:36:25 -0300

    Codigo para N semanas sin acumular las series

--- series.c
+++ series.c
@@ -8,6 +8,9 @@
 
 float TseriesD;
 float TseriesB;  
+float StseriesD = 0;
+float StseriesB = 0;
+
 float TD = 0;
 float TB = 0;
 float t[] = {0.5, 1.0, 1.5, 2.0};
@@ -16,9 +19,11 @@ float TdeCadaProfesorB[6];
 float sumatoriaD = 0;
 float sumatoriaB = 0;
 int Bool = 0;
-
+int Nsemana = 1;
 sem_t semaforo;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexdebool = PTHREAD_MUTEX_INITIALIZER;
+
 // Función que retorna un valor aleatorio del arreglo t[]
 float t_serie(void){
     int indice = rand() % 4;
@@ -59,15 +64,19 @@ void *verserie(void *arg){
         }
 
     }   
+    pthread_mutex_lock(&mutexdebool);
     Bool++;
+    pthread_mutex_unlock(&mutexdebool);
+
     if(Bool == 12){
+
         printf("-------------------------------------------------------\n");
         for(int i = 0; i < 6; i++){
-            printf("El profesor %d vio Dasney por %.1f segundos\n", i + 1, TdeCadaProfesorD[i]);
+            printf("El profesor %d vio %.1f series de Dasney\n", i + 1, TdeCadaProfesorD[i]);
         }
         printf("-------------------------------------------------------\n");
         for (int i = 0; i < 6; i++){
-            printf("El profesor %d vio Betflix por %.1f segundos\n", i + 1, TdeCadaProfesorB[i]);
+            printf("El profesor %d vio %.1f series de Betflix\n", i + 1, TdeCadaProfesorB[i]);
         }
         printf("-------------------------------------------------------\n");
         for(int i = 0; i < 6; i++){
@@ -81,6 +90,7 @@ void *verserie(void *arg){
         printf("-------------------------------------------------------\n");
         printf("Quedan %.1f series de Dasney\n", TseriesD);
         printf("Quedan %.1f series de Betflix\n", TseriesB);
+
     }
     
     pthread_mutex_unlock(&mutex);
@@ -91,13 +101,12 @@ void *verserie(void *arg){
 }
 
 int main(void){
-    sem_init(&semaforo, 0, 1);
-    srand(time(NULL)); 
+    srand(time(NULL));
+
     pthread_t PD[6];  // Profesores que ven Dasney
     pthread_t PB[6];  // Profesores que ven Betflix
     
-    TseriesD = c_series();
-    TseriesB = c_series();
+
     int N = 0;
     int Ctiempo = 0;
     printf("Ingrese el tiempo:\n");
@@ -106,7 +115,7 @@ int main(void){
     printf("[3] 1 Año\n");
 
     scanf("%d", &N);
-    if(N < 0 || N > 3){
+    if(N <= 0 || N > 3){
         while(1){
             printf("El número debe ser mayor a 0 y menor a 3\n");
             printf("Ingrese el tiempo:\n");
@@ -123,24 +132,41 @@ int main(void){
     }else if(N == 3){
         Ctiempo = 48;
     }
+
     printf("Tiempo: %d\n", Ctiempo);
-
-    printf("Series de Dasney: %.1f\n", TseriesD);
-    printf("Series de Betflix: %.1f\n", TseriesB);
     pthread_mutex_init(&mutex, NULL);
-    for(int i = 0; i < 6; i++){
-        int *idD = malloc(sizeof(int));  // Reservar memoria para cada id
-        int *idB = malloc(sizeof(int));  
-        *idD = i + 1; 
-        *idB = i + 7;
-        pthread_create(&PD[i], NULL, verserie, (void *)idD);
-        pthread_create(&PB[i], NULL, verserie, (void *)idB);
+    pthread_mutex_init(&mutexdebool, NULL);
+
+    while(Ctiempo > 0){
+        Bool = 0;
+        sumatoriaB = 0;
+        sumatoriaD = 0;
+        TseriesD = c_series();
+        TseriesB = c_series();
+        printf("-------------------------------------------------------\n");
+        printf("Semana %d\n", Nsemana);
+        printf("-------------------------------------------------------\n");
+
+        printf("Series de Dasney: %.1f\n", TseriesD);
+        printf("Series de Betflix: %.1f\n", TseriesB);
+
+        for(int i = 0; i < 6; i++){
+            int *idD = malloc(sizeof(int));  // Reservar memoria para cada id
+            int *idB = malloc(sizeof(int));  
+            *idD = i + 1; 
+            *idB = i + 7;
+            pthread_create(&PD[i], NULL, verserie, (void *)idD);
+            pthread_create(&PB[i], NULL, verserie, (void *)idB);
+        }
+
+        for(int i = 0; i < 6; i++){
+            pthread_join(PD[i], NULL);
+            pthread_join(PB[i], NULL);
+        }
+        Ctiempo--;
+        Nsemana++;
     }
 
-    for(int i = 0; i < 6; i++){
-        pthread_join(PD[i], NULL);
-        pthread_join(PB[i], NULL);
-    }
 
     return 0;
 }
